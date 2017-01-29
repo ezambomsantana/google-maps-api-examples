a9e80be75f7e925619d3a69436bec8e9e7598992
ezambomsantana <ezambomsantana@gmail.com>
2017-01-29 11:41:21 -0200

    Update EncontrarEndereco.java

--- src/gmaps/EncontrarEndereco.java
+++ src/gmaps/EncontrarEndereco.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gmaps;
 
 import com.google.maps.GeoApiContext;
@@ -10,26 +5,23 @@ import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
 import com.google.maps.model.LatLng;
 
-/**
- *
- * @author ezamb
- */
 public class EncontrarEndereco {
 
     /**
-     * @param args the command line arguments
+     * Parte 1 - Passa um endereço qualquer, e o maps responde com o endereço formatado caso ele seja encontrado.
+     * Parte 2 - Passa uma Lat/Long qualquer, e o maps responde com o endereço correspondente.
      */
     public static void main(String[] args) throws Exception {
+        
+        // substituir a String "chave" pela chave gerada na API do Google.
         GeoApiContext context = new GeoApiContext().setApiKey("chave");
-        GeocodingResult[] results =  GeocodingApi.geocode(context,
-            "Rua Casa do Ator").await();
-        System.out.println(results[0].formattedAddress);
+        GeocodingResult[] results =  GeocodingApi.geocode(context, "Rua Casa do Ator").await(); //Passa um endereço
+        System.out.println(results[0].formattedAddress); // Recebe como resposta o endereço completo, caso ele exista
         
-        LatLng latlng = new LatLng(-23.579248, -46.728165);
+        LatLng latlng = new LatLng(-23.579248, -46.728165); // Uma Lat/Lng qualquer
         
-        results =  GeocodingApi.reverseGeocode(context,
-            latlng).await();
-        System.out.println(results[0].formattedAddress);
+        results =  GeocodingApi.reverseGeocode(context, latlng).await(); // Busca o endereço dessa posição
+        System.out.println(results[0].formattedAddress); // Escreve o endereço encontrado
         
     }
     
