4e1e34dd5acdb8d1fabc0ebbb9fd71c94bf19543
ezambomsantana <ezambomsantana@gmail.com>
2017-01-29 11:38:38 -0200

    Update Distancia.java

--- src/gmaps/Distancia.java
+++ src/gmaps/Distancia.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gmaps;
 
 import com.google.maps.DistanceMatrixApi;
@@ -12,21 +7,20 @@ import com.google.maps.model.DistanceMatrix;
 
 /**
  *
- * @author ezamb
+ * @author Eduardo Santana
  */
 public class Distancia {
-        /**
-     * @param args the command line arguments
-     */
+        
     public static void main(String[] args) throws Exception {
         
+        // O aluno deve substituir a String "chave" pela chave gerada no Google.
         GeoApiContext context = new GeoApiContext().setApiKey("chave");
         
-        String [] origem = { "São Paulo" }; // origem da viagem
-        String [] destino = { "Lins" }; // destino da viagem
+        String [] origem = { "São Paulo" }; // origem da viagem, pode ser um endereço completo ou apenas o nome de uma cidade.
+        String [] destino = { "Lins" }; // destino da viagem, mesma coisa da origem.
         
         DistanceMatrixApiRequest request =  DistanceMatrixApi.getDistanceMatrix(context, origem, destino );        
-        DistanceMatrix results = request.await(); // faz a requisição no Google Maps      
+        DistanceMatrix results = request.await(); // faz a requisição no Google Maps e recupera a resposta     
         
         System.out.println(results.rows[0].elements[0].distance); // Mostra a distância entre a origem e o destino
         System.out.println(results.rows[0].elements[0].duration); // mostra o tempo estimado da viagem entre a origem e o destino
